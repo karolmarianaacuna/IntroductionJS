044fd85b81fe55a0788e1d6b223330e66b569332
Karol Mariana Acuña Barrera <165958325+karolmarianaacuna@users.noreply.github.com>
2024-08-08 17:15:33 -0500

    seal and freeze

--- js/10.js
+++ js/10.js
@@ -10,8 +10,8 @@ const Producto = {
 
 
 
-/* 
 
+/* 
 Object.freeze(Producto);
 Producto.image = "imagen.jpg";
 console.log(Producto)
@@ -20,6 +20,13 @@ console.log(Producto)
 Object.freeze() -> Este metodo sirve para cuando queremos que el objeto no acepte y sea extriction 
 en la eliminacion modificacion de los atributos de la clase
 
-*/
+
+
+ */
+
+Object.seal(Producto); /* > Este metodo permite la edicion de los atributos pero le da una seguridad a el objeto   */
+Producto.nombreProducto = "imagen.jpg";
+
+console.log(Producto.nombreProducto)
 
 
